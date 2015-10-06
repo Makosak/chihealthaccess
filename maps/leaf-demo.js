1ebf1cf4573ec24339f4cdfede5b20be69a474c9
Marynia <marynia.kolak@gmail.com>
2015-10-06 11:42:18 -0700

    first commit

---
+++ maps/leaf-demo.js
@@ -0,0 +1,27 @@
+var map = L.map( 'map', {
+    center: [41.879250, -87.631219],
+    minZoom: 2,
+    zoom: 2
+});
+
+L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+}).addTo( map );
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+ 
+var myIcon = L.icon({
+    iconUrl: myURL + 'images/pin24.png',
+    iconRetinaUrl: myURL + 'images/pin48.png',
+    iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14]
+});
+ 
+for ( var i=0; i < markers.length; ++i ) 
+{
+   L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+      .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+      .addTo( map );
+}
