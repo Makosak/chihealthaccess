582ba3c582e92a19c6678e0728af9c1a0ef6af25
Marynia <marynia.kolak@gmail.com>
2015-12-06 22:45:43 -0700

    menu updates

--- maps/leaf-demo.js
+++ maps/leaf-demo.js
@@ -20,6 +20,29 @@ var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{
   ext: 'png'
 }).addTo( map );
 
+
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
 ////////////////////////////////////////////////////////
 // Base layer is always on: City of Chicago Boundary
 ////////////////////////////////////////////////////////
@@ -85,20 +108,9 @@ function hisStyle(feature) { //Parks with more acres have darker color
     };
 }
 
-function hisColor(a) { //Parks with more acres have darker color
-    
-    return a > 60  ? '#006837' :
-           a > 45  ? '#31a354' :
-           a > 30   ? '#78c679' :
-           a > 15   ? '#c2e699' :
-           a > 0   ? '#ffffcc' :
-                      '#FFEDA0';
-}
-
-
-function coiStyle(feature) { //Parks with more acres have darker color
+function hisStyleCat(feature) { //CDPH Coding
     return {
-        fillColor: coiColor(feature.properties.COI_ct),
+        fillColor: hisColorCat(feature.properties.HIS_ct),
         weight: 1,
         opacity: 1,
         color: 'white',
@@ -107,20 +119,82 @@ function coiStyle(feature) { //Parks with more acres have darker color
     };
 }
 
-function coiColor(a) { //Parks with more acres have darker color
+function hisColor(a) { //Equal intervel
     
-    return a > 0.63  ? '#016c59' :
-           a > 0.08  ? '#1c9099' :
+    return a > 60  ? '#810f7c' :
+           a > 45  ? '#8856a7' :
+           a > 30   ? '#8c96c6' :
+           a > 15   ? '#b3cde3' :
+           a > 0   ? '#edf8fb' :
+                      '#FFEDA0';
+}
+
+function hisColorCat(a) { //CDPH Coding
+  
+    return a > (46.4)  ? '#b30000' :
+           a > (33.7)   ? '#fc8d59' :
+           a > (6.7)   ? '#fef0d9' :
+                      '#f7f7f7';
+}
+
+
+function coiStyleEq(feature) { //Parks with more acres have darker color
+    return {
+        fillColor: coiColorEq(feature.properties.COI_ct),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 0.7
+    };
+}
+
+function coiStyleCat(feature) { //Parks with more acres have darker color
+    return {
+        fillColor: coiColorCat(feature.properties.COI_ct),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 0.7
+    };
+}
+
+
+function coiColorEq(a) { // Equal Interval
+    
+    return a > 0.63  ? '#f6eff7' :
+           a > 0.08  ? '#bdc9e1' :
            a > (-0.45)   ? '#67a9cf' :
-           a > (-0.99)   ? '#bdc9e1' :
-           a > (-1.54)   ? '#f6eff7' :
+           a > (-0.99)   ? '#1c9099' :
+           a > (-1.54)   ? '#016c59' :
+                      '#FFEDA0';
+}
+
+function coiColorCat(a) { // CDPH Category
+    
+    return a > (0.424)  ? '#fef0d9' :
+           a > (0.115)       ? '#fdcc8a' :
+           a > (-0.11279)   ? '#fc8d59' :
+           a > (-0.40195)        ? '#e34a33' :
+           a > (-1.54)   ? '#b30000' :
                       '#FFEDA0';
 }
 
 
+/*
+coiScale
+
+min -1.54 
+max 1.17
+
+
+*/
+
 function highlightFeature(e) {
     var layer = e.target;
 
+    var props = layer.feature.properties;
     info.update(layer.feature.properties);
 
     layer.setStyle({
@@ -142,6 +216,31 @@ function resetHighlight(e) {
     console.log("resetHighlight");
 }
 
+function highlightFeatureTract(e) {
+    var layer = e.target;
+
+    var props = layer.feature.properties;
+    info.updateTract(layer.feature.properties);
+
+    layer.setStyle({
+        weight: 2,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+        
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlightTract(e) {
+    //commAreas.layer.resetStyle(e.target);
+    layer.resetStyle(e.target);
+
+}
+
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
@@ -151,6 +250,13 @@ function onEachFeature(feature, layer) {
         mouseover: highlightFeature,
         mouseout: resetHighlight,
         doubleclick: zoomToFeature
+    });
+}
+
+function onEachFeatureTract(feature, layer) {
+    layer.on({
+        mouseover: highlightFeatureTract,
+        mouseout: resetHighlightTract
     });
 }
 
@@ -167,6 +273,57 @@ info.update = function (props) {
     this._div.innerHTML = '<h4>Community Area</h4>' +  (props ?
         '<b>' + props.COMMUNITY + ' (ID: ' + props.AREA_NUMBE + ')'
         : 'Hover over an area');
+
+    document.getElementById("testingHover").innerHTML = '<h4>Community Area</h4>' +  (props ?
+        '<b>' + props.COMMUNITY + ' (ID: ' + props.AREA_NUMBE + ')'
+        : 'Hover over an area');
+};
+
+info.updateTract = function (props) {
+    this._div.innerHTML = '<h4>Community Area ID: ' +  (props ?
+        '<b>' + props.COMMAREA + '</h4>'
+        : 'Hover over an area');
+
+    document.getElementById("testingHover").innerHTML = '<h4>Results for Selected Tract: </h4>' 
+        +  (props ? '<p>'
+        + ' Household Income Diversity Index: ' + props.HIS_ct + '<br>'
+        + ' Childhood Opportunity Index: ' + props.COI_ct + '<br>'
+        + ' Years Lost: ' + props.YEARS_LOST + '<br>'
+        + ' Population in 2012: ' + props.Pop2012 + '<br>'
+        + ' Population Change (since 2010): ' + props.PopChange + '<br>'
+
+
+        : 'Hover over an area');
+
+    var data = [];
+    var features = COI2tracts.geojson.features;
+
+    for (var i = 0; i < features.length; i++) {
+      data.push(features[i].properties.COI_ct);
+    };
+
+
+
+
+    // console.log(d3.min(data), d3.max(data));
+
+    var x = d3.scale.linear()
+        .domain([d3.min(data), d3.max(data)])
+        .range([0, 420]);
+
+    data = d3.layout.histogram()
+    .bins(x.ticks(5))
+    (data);
+
+
+    d3.select(".chart")  //select chart container using a class selector
+      .selectAll("div")  //initiate data join by defining selection to which we join the data
+        .data(data) // join the data
+
+        
+      .enter().append("div")
+        .style("width", function(d) { return x(d) + "px"; })
+        .text(function(d) { return d; });
 };
 
 info.addTo(map);
@@ -249,6 +406,7 @@ function loadHIS(){
       HIStracts.layer = L.geoJson(geojson, 
       {
           style: hisStyle, 
+          onEachFeature: onEachFeatureTract 
 
       }).addTo(map);
       HIStracts.geojson = geojson;
@@ -268,33 +426,99 @@ function loadHIS(){
 
 }
 
-var COItracts = {},
-COItractsLoaded = [false, false]; //[init(button click at all), on/off]
 
-function loadCOI(){
-  if(!COItractsLoaded[0]){
+var HIS2tracts = {},
+HIS2tractsLoaded = [false, false]; //[init(button click at all), on/off]
+
+
+function loadHIS2(){
+  if(!HIS2tractsLoaded[0]){
     shp("./data/CDPHTractsFinal_Clipped").then(function(geojson){
-      COItracts.layer = L.geoJson(geojson, 
+      HIS2tracts.layer = L.geoJson(geojson, 
       {
-          style: coiStyle, 
+          style: hisStyleCat, 
+          onEachFeature: onEachFeatureTract  
 
       }).addTo(map);
-      COItracts.geojson = geojson;
-      COItractsLoaded = [true, true];
+      HIS2tracts.geojson = geojson;
+      HIS2tractsLoaded = [true, true];
     });
   }
   else{
-    if(COItractsLoaded[1]){
-      reloadExistingLayers(COItracts);
-      COItractsLoaded[1] = false;
+    if(HIS2tractsLoaded[1]){
+      reloadExistingLayers(HIS2tracts);
+      HIS2tractsLoaded[1] = false;
     }
     else{
-      COItracts.layer.addTo(map);
-      COItractsLoaded[1] = true;
+      HIS2tracts.layer.addTo(map);
+      HIS2tractsLoaded[1] = true;
     }
   }
 
 }
+
+var COI1tracts = {},
+COI1tractsLoaded = [false, false]; //[init(button click at all), on/off]
+
+function loadCOI1(){
+  if(!COI1tractsLoaded[0]){
+    shp("./data/CDPHTractsFinal_Clipped").then(function(geojson){
+      COI1tracts.layer = L.geoJson(geojson, 
+      {
+          style: coiStyleEq, 
+          onEachFeature: onEachFeatureTract 
+
+      }).addTo(map);
+      COI1tracts.geojson = geojson;
+      COI1tractsLoaded = [true, true];
+    });
+  }
+  else{
+    if(COI1tractsLoaded[1]){
+      reloadExistingLayers(COI1tracts);
+      COI1tractsLoaded[1] = false;
+    }
+    else{
+      COI1tracts.layer.addTo(map);
+      COI1tractsLoaded[1] = true;
+    }
+  }
+
+}
+
+
+var COI2tracts = {},
+COI2tractsLoaded = [false, false]; //[init(button click at all), on/off]
+
+function loadCOI2(){
+  if(!COI2tractsLoaded[0]){
+    shp("./data/CDPHTractsFinal_Clipped").then(function(geojson){
+      COI2tracts.layer = L.geoJson(geojson, 
+      {
+          style: coiStyleCat, 
+          onEachFeature: onEachFeatureTract 
+
+      }).addTo(map);
+      COI2tracts.geojson = geojson;
+      COI2tractsLoaded = [true, true];
+    });
+  }
+  else{
+    if(COI2tractsLoaded[1]){
+      reloadExistingLayers(COI2tracts);
+      COI2tractsLoaded[1] = false;
+    }
+    else{
+      COI2tracts.layer.addTo(map);
+      COI2tractsLoaded[1] = true;
+    }
+  }
+
+}
+
+
+
+
 
 ////////////////////////////////////////////////////////
 // Reset: take off active layer and show base map layer
@@ -308,5 +532,4 @@ function reloadExistingLayers(geojson){
   //geo.clearLayers();
   //L.geoJson(cityBoundary).addTo(map);
 }
-
 
